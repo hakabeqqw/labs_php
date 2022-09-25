b9ec3f6ac9f745467f995dea39e12e95e2828d7a
hakabe <makasanmr300@gmail.com>
2022-09-25 14:44:54 +0300

    fix(lab1): input restriction

--- lab1/calculator.php
+++ lab1/calculator.php
@@ -1,13 +1,13 @@
 <?php
 
-$expression = '4+3+5-6';
+$expression = $argv[1];
 
 function calculator(string $expression): string
 {
     $correctString = ' +-1234567890';
     for ($i = 0; $i < mb_strlen($expression); $i++) {
         if (!strpos($correctString, $expression[$i])) {
-            return "incorrect";
+            return "неверно";
         }
     }
 
@@ -15,13 +15,19 @@ function calculator(string $expression): string
 
     $process = $counting[0];
 
+    $countSigns = 0;
 
     for ($i = 1; $i < mb_strlen($expression); $i++) {
         if ($counting[$i] === '+') {
             $process = $process + $expression[$i + 1];
+            $countSigns++;
         } elseif ($counting[$i] === '-') {
             $process = $process - $expression[$i + 1];
+            $countSigns++;
         }
+    }
+    if ($countSigns > 4) {
+        return "введено больше 5 слогаемых";
     }
     return $process;
 }
