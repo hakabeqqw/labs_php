46b2591fc1e67dad86ab50df43d5d6e7361404c6
hakabe <makasanmr300@gmail.com>
2022-09-25 21:33:49 +0300

    fix(lab2): solving hours

--- lab2/sumTime.php
+++ lab2/sumTime.php
@@ -5,12 +5,12 @@ function sumTime(string $time1, string $time2): string
 {
     $correctString = ' :1234567890';
     for ($i = 0; $i < strlen($time1); $i++) {
-        if (!strpos($correctString, $time1[$i]) || strlen($time1) != 8) {
+        if (!strpos($correctString, $time1[$i]) || strlen($time1) !== 8) {
             return 'введите 1 время, согласно примеру:"10:20:30" или "01:05:30"';
         }
     }
     for ($i = 0; $i < strlen($time2); $i++) {
-        if (!strpos($correctString, $time2[$i]) || strlen($time2) != 8) {
+        if (!strpos($correctString, $time2[$i]) || strlen($time2) !== 8) {
             return 'введите 2 время, согласно примеру:"10:20:30" или "01:05:30"';
         }
     }
@@ -19,8 +19,10 @@ function sumTime(string $time1, string $time2): string
     $hour2 = mb_substr($time2, 0, 2);
     $hour2 = +$hour2;
     $sumHours = $hour1 + $hour2;
-    if ($sumHours > 24) {
-        $sumHours = $sumHours - 24;
+    for (; $sumHours > 24;) {
+        if ($sumHours > 24) {
+            $sumHours = $sumHours - 24;
+        }
     }
     $minute1 = mb_substr($time1, 3, 5);
     $minute1 = +$minute1;
@@ -40,7 +42,7 @@ function sumTime(string $time1, string $time2): string
         $sumSeconds = $sumSeconds - 60;
         $sumMinutes = $sumMinutes + 1;
     }
-    return $sumHours .":". $sumMinutes .":". $sumSeconds;
+    return $sumHours . ":" . $sumMinutes . ":" . $sumSeconds;
 }
 
 echo sumTime($time1, $time2);
