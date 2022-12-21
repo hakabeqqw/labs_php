c023c961a680cc202ba38aaaaa51194fb35fbd6c
hakabeqqw <makasanmr300@gmail.com>
2022-12-21 17:10:51 +0300

    fix(lw2): made formatting

--- lw2/src/ChocolateFactory.php
+++ lw2/src/ChocolateFactory.php
@@ -3,11 +3,11 @@ require_once 'Snack.php';
 
 abstract class ChocolateFactory
 {
-	abstract protected function createSnack(string $type): Snack;
-	public function orderSnack(string $type): void
-	{
-		$snack = $this->createSnack($type);
-		$snack->prepare();
-		$snack->cut();
-	}
+    abstract protected function createSnack(string $type): Snack;
+    public function orderSnack(string $type): void
+    {
+        $snack = $this->createSnack($type);
+        $snack->prepare();
+        $snack->cut();
+    }
 }
